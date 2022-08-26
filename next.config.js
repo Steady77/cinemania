1cf608e1aa2ebb59724c1db9237c376f00cefff7
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-08-26 21:25:36 +0300

    chore: setting up the environment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  poweredByHeader: false,
+  optimizeFonts: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
