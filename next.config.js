584832e69fa2eb613de591089837937eb124f752
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-09-01 21:43:43 +0300

    chore: change next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   poweredByHeader: false,
   optimizeFonts: false,
   env: {
+    APP_URL: process.env.REACT_APP_APP_URL,
     API_URL: process.env.REACT_APP_API_URL,
     API_KEY: process.env.REACT_APP_API_KEY,
     APP_ENV: process.env.REACT_APP_ENV,
