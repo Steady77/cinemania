8b43c9842349e50a7a17e69bb88cd666cd4872b0
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-09-03 19:59:28 +0300

    chore: add images domain to next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['kinopoiskapiunofficial.tech'],
+  },
   poweredByHeader: false,
   optimizeFonts: false,
   env: {
