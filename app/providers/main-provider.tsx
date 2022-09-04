6bfb1ee496f327817ea7e1ad1eb4d83a388a7c81
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-09-04 19:37:03 +0300

    feat: customize react-redux-toastr

--- app/providers/main-provider.tsx
+++ app/providers/main-provider.tsx
@@ -1,7 +1,12 @@
 import { FC, PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 
 import Layout from '@/components/layout/layout';
+
+import { store } from '@/store/store';
+
+import ReduxToast from './redux-toast';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,9 +18,12 @@ const queryClient = new QueryClient({
 
 const MainProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layout>{children}</Layout>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ReduxToast />
+        <Layout>{children}</Layout>
+      </QueryClientProvider>
+    </Provider>
   );
 };
 export default MainProvider;
