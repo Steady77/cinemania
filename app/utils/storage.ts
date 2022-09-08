645518c3c1b855f683d4cbbe01b661efe0cec1b6
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-09-08 18:14:45 +0300

    feat: add functions for working with storage

---
+++ app/utils/storage.ts
@@ -0,0 +1,18 @@
+import { IUserCred } from '@/store/auth/auth.interface';
+
+export const getFromLS = (name: string) => {
+  if (typeof name !== 'undefined') {
+    const ls = localStorage.getItem(name);
+    return ls ? JSON.parse(ls) : null;
+  }
+
+  return null;
+};
+
+export const saveToLS = (name: string, data: IUserCred) => {
+  localStorage.setItem(name, JSON.stringify(data));
+};
+
+export const removeFromLS = (name: string) => {
+  localStorage.removeItem(name);
+};
