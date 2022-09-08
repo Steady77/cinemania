47655976ca3375f579a376e4deb3ac5433720438
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-09-08 19:51:27 +0300

    chore: add breakpoints to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,6 +81,12 @@ module.exports = {
         scaleIn: 'scaleIn .35s ease-in-out',
       },
     },
+    screens: {
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' },
+    },
   },
   plugins: [
     plugin(({ addComponents, theme, addUtilities }) => {
