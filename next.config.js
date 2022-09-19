56803c1096d863e94fe8eeb1e87ab9c9db3e23d1
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-09-19 19:38:15 +0300

    chore: change next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['kinopoiskapiunofficial.tech'],
-  },
-  poweredByHeader: false,
-  optimizeFonts: false,
-  env: {
-    APP_URL: process.env.REACT_APP_APP_URL,
-    API_URL: process.env.REACT_APP_API_URL,
-    API_KEY: process.env.REACT_APP_API_KEY,
-    APP_ENV: process.env.REACT_APP_ENV,
-  },
-  swcMinify: true,
+	images: {
+		domains: ['kinopoiskapiunofficial.tech'],
+	},
+	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.REACT_APP_APP_URL,
+		API_URL: process.env.REACT_APP_API_URL,
+		API_KEY: process.env.REACT_APP_API_KEY,
+		APP_ENV: process.env.REACT_APP_ENV,
+		SERVER_URL: process.env.REACT_APP_SERVER_URL,
+	},
+	swcMinify: true,
 };
 
 module.exports = nextConfig;
