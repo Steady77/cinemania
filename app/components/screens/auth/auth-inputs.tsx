8d03a9bb514b7f8f905e18708e3fe8e965f4bf30
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-10-17 19:45:43 +0300

    fix: set correct type for auth form

--- app/components/screens/auth/auth-inputs.tsx
+++ app/components/screens/auth/auth-inputs.tsx
@@ -5,48 +5,50 @@ import Input from '@/components/ui/form-elements/input';
 
 import { validEmail } from '@/shared/regex';
 
+import { IAuthInput } from './auth.interface';
+
 interface IAuthInputs {
-  register: UseFormRegister<any>;
-  formState: FormState<any>;
-  isPasswordRequired: boolean;
+	register: UseFormRegister<any>;
+	formState: FormState<IAuthInput>;
+	isPasswordRequired: boolean;
 }
 
 const AuthInputs: FC<IAuthInputs> = ({
-  formState: { errors },
-  isPasswordRequired = false,
-  register,
+	formState: { errors },
+	isPasswordRequired = false,
+	register,
 }) => {
-  return (
-    <>
-      <Input
-        {...register('email', {
-          required: 'Введите почту',
-          pattern: {
-            value: validEmail,
-            message: 'Введите корректный адрес почты',
-          },
-        })}
-        placeholder="Почта"
-        error={errors.email}
-      />
-      <Input
-        {...register(
-          'password',
-          isPasswordRequired
-            ? {
-                required: 'Введите пароль',
-                minLength: {
-                  value: 6,
-                  message: 'Введите более 6 символов',
-                },
-              }
-            : {},
-        )}
-        placeholder="Пароль"
-        type={'password'}
-        error={errors.password}
-      />
-    </>
-  );
+	return (
+		<>
+			<Input
+				{...register('email', {
+					required: 'Введите почту',
+					pattern: {
+						value: validEmail,
+						message: 'Введите корректный адрес почты',
+					},
+				})}
+				placeholder="Почта"
+				error={errors.email}
+			/>
+			<Input
+				{...register(
+					'password',
+					isPasswordRequired
+						? {
+								required: 'Введите пароль',
+								minLength: {
+									value: 6,
+									message: 'Введите более 6 символов',
+								},
+						  }
+						: {},
+				)}
+				placeholder="Пароль"
+				type={'password'}
+				error={errors.password}
+			/>
+		</>
+	);
 };
 export default AuthInputs;
--- app/components/ui/form-elements/form.interface.ts
+++ app/components/ui/form-elements/form.interface.ts
@@ -1,12 +1,11 @@
 import { ButtonHTMLAttributes, InputHTMLAttributes } from 'react';
-
-// import { FieldError } from 'react-hook-form'
+import { FieldError } from 'react-hook-form';
 
 export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export interface IInputProps {
-  placeholder: string;
-  error?: /* FieldError */ any | undefined;
+	placeholder: string;
+	error?: FieldError | undefined;
 }
 
 type InputPropsType = InputHTMLAttributes<HTMLInputElement> & IInputProps;
