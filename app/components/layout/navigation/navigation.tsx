787bbb3b8ba99b7ad829791d9322726ede382465
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-11-04 22:56:58 +0300

    perf: resolve wasted renders in the sidebar and navigation components

--- app/components/layout/navigation/navigation.tsx
+++ app/components/layout/navigation/navigation.tsx
@@ -1,16 +1,18 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 
 import Logo from './logo';
 import MenuContainer from './menu-container/menu-container';
 import styles from './navigation.module.scss';
 
-const Navigation: FC = () => {
+const Navigation: FC = memo(() => {
 	return (
 		<aside className={styles.navigation}>
 			<Logo />
 			<MenuContainer />
 		</aside>
 	);
-};
+});
+
+Navigation.displayName = 'Navigation';
 
 export default Navigation;
--- app/components/layout/sidebar/sidebar.tsx
+++ app/components/layout/sidebar/sidebar.tsx
@@ -1,15 +1,18 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 
 import MoviesContainer from './movies-container/movies-container';
 import Search from './search/search';
 import styles from './sidebar.module.scss';
 
-const Sidebar: FC = () => {
+const Sidebar: FC = memo(() => {
 	return (
 		<aside className={styles.sidebar}>
 			<Search />
 			<MoviesContainer />
 		</aside>
 	);
-};
+});
+
+Sidebar.displayName = 'Sidebar';
+
 export default Sidebar;
