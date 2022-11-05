ea37c554148d8f8145ca5d6fd6dbdebc5f929154
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-11-05 14:41:29 +0300

    fix: name for the error500 component

--- pages/500.tsx
+++ pages/500.tsx
@@ -2,11 +2,11 @@ import Header from '@/components/ui/header/header';
 
 import Meta from '@/utils/meta/meta';
 
-const Error404 = () => {
-  return (
-    <Meta title="Страница не найдена">
-      <Header title="500 - Ошибка сервера" />
-    </Meta>
-  );
+const Error500 = () => {
+	return (
+		<Meta title="Страница не найдена">
+			<Header title="500 - Ошибка сервера" />
+		</Meta>
+	);
 };
-export default Error404;
+export default Error500;
