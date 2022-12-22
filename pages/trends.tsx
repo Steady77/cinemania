77e3397b74f70de756a759c5f10f6f7eee1a701c
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-12-22 10:31:51 +0300

    feat: use static generation for trends page

--- pages/trends.tsx
+++ pages/trends.tsx
@@ -1,6 +1,9 @@
-import { NextPage } from 'next';
+import { QueryClient, dehydrate } from '@tanstack/react-query';
+import { GetStaticProps, NextPage } from 'next';
 
 import TrendsCatalog from '@/components/ui/catalogs/trends-catalog/trends-catalog';
+
+import { MovieService } from '@/services/movie.service';
 
 const TrendsPage: NextPage = () => {
 	return (
@@ -9,6 +12,28 @@ const TrendsPage: NextPage = () => {
 			description={`Топ 100 самых популярных фильмов и сериалов`}
 		/>
 	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const queryClient = new QueryClient();
+
+	try {
+		await queryClient.prefetchInfiniteQuery(
+			['popular movies'],
+			({ pageParam = 1 }) =>
+				MovieService.getTop('TOP_100_POPULAR_FILMS', pageParam),
+		);
+
+		return {
+			props: {
+				dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+			},
+		};
+	} catch (error) {
+		return {
+			notFound: true,
+		};
+	}
 };
 
 export default TrendsPage;
