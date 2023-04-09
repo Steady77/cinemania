22c83bccfe85a77894e9ce95462a3f47fdbb21da
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-04-09 17:49:18 +0300

    chore: tabs instead of spaces

--- app/components/ui/form-elements/input.tsx
+++ app/components/ui/form-elements/input.tsx
@@ -5,26 +5,26 @@ import { IInput } from './form.interface';
 import styles from './form.module.scss';
 
 const Input = forwardRef<HTMLInputElement, IInput>(
-  ({ placeholder, error, type = 'text', style, ...rest }, ref) => {
-    return (
-      <>
-        <div
-          className={cn(styles.common, styles.field)}
-          style={style}
-        >
-          <label>
-            <span>{placeholder}</span>
-            <input
-              ref={ref}
-              type={type}
-              {...rest}
-            />
-          </label>
-          {error && <div className={styles.error}>{error.message}</div>}
-        </div>
-      </>
-    );
-  },
+	({ placeholder, error, type = 'text', style, ...rest }, ref) => {
+		return (
+			<>
+				<div
+					className={cn(styles.common, styles.field)}
+					style={style}
+				>
+					<label>
+						<span>{placeholder}</span>
+						<input
+							ref={ref}
+							type={type}
+							{...rest}
+						/>
+					</label>
+					{error && <div className={styles.error}>{error.message}</div>}
+				</div>
+			</>
+		);
+	},
 );
 
 Input.displayName = 'Input';
